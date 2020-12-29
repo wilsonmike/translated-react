013338d5f8153808e2481da96aeba5e24fe63dd9
mike <michaelwilsonsj@hotmail.com>
2020-12-28 22:00:28 -0500

    added approval comment cards semantic ui

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <div className="App">
-    <h1>Translated</h1>
-  </div>;
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'; 
+import ReactDOM from 'react-dom'; 
+import faker from 'faker'; 
+import CommentDetail from './CommentDetail'; 
+import ApprovalCard from './ApprovalCard'; 
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+            <CommentDetail author="Sam"  timeAgo="Today at 4:15pm" commentText="Great Article!" image={faker.image.image()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+            <CommentDetail author="Mike" timeAgo="Today at 5:15pm" commentText="Thanks for the information" image={faker.image.image()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+            <CommentDetail author="Joe" timeAgo="Yesterday at 6:15pm" commentText="Interesting" image={faker.image.image()}/>
+            </ApprovalCard>
+        </div>
+    );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector('#root')
+)
