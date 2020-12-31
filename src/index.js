2789f79b8920193f47e395bfabc9ce8a81528bda
mike <michaelwilsonsj@hotmail.com>
2020-12-30 20:26:40 -0500

    refactor code

--- src/index.js
+++ src/index.js
@@ -16,16 +16,24 @@ class App extends React.Component {
             }
         )
     }
+
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+ 
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+ 
+        return <Spinner message="Please accept location request..." />
+    }
     render() {
-       if (this.state.errorMessage && !this.state.lat) {
-           return <div>Error: {this.state.errorMessage}</div>
-       }
-
-       if (!this.state.errorMessage && this.state.lat) {
-           return <SeasonDisplay lat={this.state.lat} />
-       }
-
-       return <Spinner />
+       return (
+           <div>
+               {this.renderContent()}
+           </div>
+       )
     }
 }
 
