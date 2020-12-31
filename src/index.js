9f24534e420c7a13d815139ffe1af1eecf054d80
mike <michaelwilsonsj@hotmail.com>
2020-12-30 19:18:42 -0500

    finished geolocation and simple styling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom'; 
+import SeasonDisplay from './SeasonDisplay';
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props); 
-
-        this.state = { lat: null, errorMessage: '' };
-
+    state = { lat: null, errorMessage: '' }; 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 this.setState({ lat: position.coords.latitude });
@@ -16,14 +14,14 @@ class App extends React.Component {
                 this.setState({ errorMessage: err.message });
             }
         )
-    };
+    }
     render() {
        if (this.state.errorMessage && !this.state.lat) {
            return <div>Error: {this.state.errorMessage}</div>
        }
 
        if (!this.state.errorMessage && this.state.lat) {
-           return <div>Latitude: {this.state.lat}</div>
+           return <SeasonDisplay lat={this.state.lat} />
        }
 
        return <div>Loading!</div>
